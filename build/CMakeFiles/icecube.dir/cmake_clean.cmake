628490fae1a2b7136562b684854464fdb9bc04fb
Joseph Pinon <josephgpinon@gmail.com>
2024-03-23 14:40:04 -0500

    init

---
+++ build/CMakeFiles/icecube.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icecube.dir/src/glad.c.o"
+  "CMakeFiles/icecube.dir/src/glad.c.o.d"
+  "CMakeFiles/icecube.dir/src/main.cpp.o"
+  "CMakeFiles/icecube.dir/src/main.cpp.o.d"
+  "icecube"
+  "icecube.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/icecube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
