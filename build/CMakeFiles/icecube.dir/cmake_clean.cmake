61fa8bb813c7f16b71252293a0fc7b692ff0b7c6
Joseph Pinon <josephgpinon@gmail.com>
2024-04-01 18:35:46 -0500

    modified gitignore

--- build/CMakeFiles/icecube.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/icecube.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/icecube.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/icecube.dir/src/main.cpp.o: \
- /home/joseph/documents/cfd/icecube/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/joseph/documents/cfd/icecube/src/application.h
